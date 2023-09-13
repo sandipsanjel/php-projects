604153f0c1dc27a5fda6acf83c4a33df188fb0a6
sandip <sandipsanjel@users.noreply.github.com>
2023-09-13 23:22:03 +0545

    first

---
+++ email verification/home.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title> Home</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <style>
+    @import url('https://fonts.googleapis.com/css?family=Poppins:400,500,600,700&display=swap');
+   
+    </style>
+</head>
+<body>
+    <nav class="navbar">
+    <a class="navbar-brand" href="#">Brand name</a>
+    <button type="button" class="btn btn-light"><a href="#">Logout</a></button>
+    </nav>
+    <h1>Welcome </h1>
+    
+</body>
+</html>
