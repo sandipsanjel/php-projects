422795058511118b6daf30a937df493b0ceb5321
sandip <sandipsanjel@users.noreply.github.com>
2023-09-15 23:29:02 +0545

    added ajax and php

--- chatbot/message.php
+++ chatbot/message.php
@@ -2,8 +2,12 @@
 $conn=mysqli_connect("localhost","root","","chatbot")
 or die("databse error");
 
-//getting message through ajax
+// getting message through ajax
 $getMesg=mysqli_real_escape_string($conn, $_POST['text']);
+
+$check_data="SELECT replies FROM chat WHERE queries LIKE '%getMesg'";
+$run_query=mysqli_query($conn, $check_data)or die ("Error");
+
 ?>
 
 
